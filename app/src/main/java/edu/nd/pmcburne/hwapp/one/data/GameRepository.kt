9e2cb07346f923fd4083050da24b4b611b86fd3e
Grace Zhang <gracezhang5175@gmail.com>
2026-03-13 15:50:00 -0400

    Created repository to fetch API data. Dynamic

--- app/src/main/java/edu/nd/pmcburne/hwapp/one/data/GameRepository.kt
+++ app/src/main/java/edu/nd/pmcburne/hwapp/one/data/GameRepository.kt
@@ -1,4 +1,22 @@
 package edu.nd.pmcburne.hwapp.one.data
 
+import edu.nd.pmcburne.hwapp.one.network.RetrofitInstance
+
 class GameRepository {
+
+    private val api = RetrofitInstance.api
+
+    suspend fun getGames(
+        gender: String,
+        year: Int,
+        month: Int,
+        day: Int
+    ): GamesResponse {
+        return api.getGames(
+            gender = gender,
+            year = year.toString(),
+            month = month.toString(),
+            day = day.toString()
+        )
+    }
 }
