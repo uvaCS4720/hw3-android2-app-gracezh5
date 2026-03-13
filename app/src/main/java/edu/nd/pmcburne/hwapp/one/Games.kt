e73baa229d23cb7eb0dd2f74faf4b92a1705054b
Grace Zhang <gracezhang5175@gmail.com>
2026-03-13 15:37:23 -0400

    Data layer - updated GameModels for API data class

--- app/src/main/java/edu/nd/pmcburne/hwapp/one/Games.kt
+++
@@ -1,8 +0,0 @@
-package edu.nd.pmcburne.hwapp.one
-
-data class ScoreboardResponse(
-    val games: List<Game>
-)
-
-class Games {
-}
---
+++ app/src/main/java/edu/nd/pmcburne/hwapp/one/data/GameModels.kt
@@ -0,0 +1,42 @@
+package edu.nd.pmcburne.hwapp.one.data
+
+data class GamesResponse(
+    val inputMD5Sum: String,
+    val instanceId: String,
+    val updated_at: String,
+    val games: List<GameWrapper>
+)
+
+data class GameWrapper(
+    val game: Game
+)
+
+data class Game(
+    val gameID: String,
+    val home: Team,
+    val away: Team,
+    val startTime: String,
+    val startDate: String,
+    val gameState: String,
+    val finalMessage: String
+)
+
+data class Team(
+    val score: String,
+    val names: TeamNames,
+    val winner: Boolean,
+    val seed: String,
+    val conferences: List<Conference>
+)
+
+data class TeamNames(
+    val char6: String,
+    val short: String,
+    val seo: String,
+    val full: String
+)
+
+data class Conference(
+    val conferenceName: String,
+    val conferenceSeo: String
+)
